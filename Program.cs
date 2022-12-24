07fa8ff1f61029da35139b800f10c8b9c969b521
Rashmi <rashmichigon65@gmail.com>
2022-12-24 12:54:33 +0530

    UserRegistrationProblem

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Day19_UserRegistrationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome To Regex User Registration Problem");
+            RegexClass code = new RegexClass();
+            Console.WriteLine("1 - Enter First Name");
+            Console.WriteLine("2 - Enter Last Name");
+            int select = Convert.ToInt32(Console.ReadLine());
+            switch (select)
+            {
+                case 1:
+                    Console.WriteLine("Enter First Name");
+                    string firstName = Console.ReadLine();
+                    code.name(firstName);
+                    break;
+
+            }
+
+
+        }
+    }
+    }
---
+++ RegexClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day19_UserRegistrationProblem
+{
+    internal class RegexClass
+    {
+
+        public void name(string name)
+        {
+            const string NAME = "^[A-Z]{1}[a-z]{2,}$";
+
+            if (RegexClass.IsMatch(name, NAME))
+            {
+                Console.WriteLine(name + " is a valid Name");
+                return;
+            }
+            Console.WriteLine(name + " is a invalid Name");
+            return;
+        }
+
+        private static bool IsMatch(string name, string nAME)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
